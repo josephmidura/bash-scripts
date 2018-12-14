951e0f3dc27f084d23cddc4a5f128cdd6d303762
Joe Midura <josephmidura@utexas.edu>
2018-12-14 10:42:58 -0600

    improve terminal message

--- brewup.sh
+++ brewup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# I should update and use this more often   
+
 # 
-echo 'this will update homebrew'
+echo 'Updating homebrew...'
 brew update      # update local base of available packages and versions
 brew upgrade     # install new versions of outdated packages        
 brew cleanup -s  # keep only linked versions of software
