6beea05fe7de649d917835de71af3528cf34ce06
Joe Midura <josephmidura@gmail.com>
2019-02-06 11:03:58 -0600

    update filename

--- brewup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# 
-echo 'Updating homebrew...'
-brew update       # update local base of available packages and versions
-brew upgrade      # install new versions of outdated packages        
-brew cleanup      # cache cleanup of unfinished downloads
-# now diagnotics
-brew doctor       # show problems
