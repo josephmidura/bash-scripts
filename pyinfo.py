c435ada506b6e107344b0a54ed34bbdbef73a212
J <josephmidura@gmail.com>
2020-12-30 12:53:40 -0600

    initial commit

---
+++ pyinfo.py
@@ -0,0 +1,38 @@
+# Display information about the python version and OS
+# Last update 12/30/20
+
+import platform as pl
+
+profile = [
+    'architecture',
+    'linux_distribution', # If I'm on Linxu
+    'mac_ver',            # If I'm on mac
+    'machine',
+    'node',
+    'platform',
+    'processor',
+    'python_build',
+    'python_compiler',
+    'python_version',
+    'release',
+    'system',
+    'uname',
+    'version',
+]
+
+class bcolors:
+    HEADER    = '\033[95m'
+    OKBLUE    = '\033[94m'
+    OKGREEN   = '\033[92m'
+    WARNING   = '\033[93m'
+    FAIL      = '\033[91m'
+    ENDC      = '\033[0m'
+    BOLD      = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+for key in profile:
+    if hasattr(pl, key):
+        print(key + bcolors.BOLD + ": " + str(getattr(pl, key)()) + bcolors.ENDC)
+
+
