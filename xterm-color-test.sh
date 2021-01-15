9cd6bdb90f81d16e9a8b11f3c2a1e7eab9c5818c
J <josephmidura@gmail.com>
2021-01-15 12:13:12 -0600

    formatting updates

--- xterm-color-test.sh
+++ xterm-color-test.sh
@@ -1,6 +1,5 @@
 # define terminal colors
-# last update 12/31/20
-
+# last update 01/15/21
  
 # Define a few colors (with nonprinting escapes included)
 BLACK='\[\e[0;30m\]'        # Black
@@ -28,12 +27,12 @@ UND='\[\e[4m\]'             # Underline
 BLINK='\[\e[5m\]'           # Text blinking
 NC='\[\e[0m\]'              # No Color
 
-## Old style
+# Old style
 PS1="\[\e[1;31m\]\u \[\e[1;32m\]\d \[\e[4;32m\]\@\[\e[0;1;33m\] [\w]\n\
 \[\e[0;35m\]  hist: \[\e[1;36m\]\! \[\e[0;35m\]cmd: \[\e[1;36m\]\#\
 \[\e[1;34m\] > \$ \[\e[0m\]"
 
-## New ssing variables
+# New using variables
 PS1="$NC\n$LRED\u $GREEN\d $UND\@$NC $YELLOW[\w]\n\
 $PURPLE  hist: $CYAN\!$PURPLE cmd: $CYAN\# $LBLUE> \$ $NC"
 
